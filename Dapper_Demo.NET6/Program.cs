06d530742531f974442b86d86b894196e803beaf
johndward01 <johndward01@gmail.com>
2022-08-01 10:57:22 -0500

    update

--- Dapper_Demo.NET6/Program.cs
+++ Dapper_Demo.NET6/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+string connString = config.GetConnectionString("DefaultConnection");
+IDbConnection conn = new MySqlConnection(connString);
+
+
